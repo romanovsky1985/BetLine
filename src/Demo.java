9370ae070023d2b87755c88efd104a184bd488e5
Anton Romanovsky <anton.romanovsky.1985@gmail.com>
2024-09-22 12:25:18 +0300

    fix merge

--- src/Demo.java
+++ src/Demo.java
@@ -3,35 +3,18 @@ import betline.core.LineBuilder;
 import betline.sport.icehockey.IceHockeyEvent;
 import betline.sport.icehockey.IceHockeyGame;
 import betline.sport.icehockey.IceHockeyGenerator;
-<<<<<<< HEAD:src/betline/Test.java
 import betline.sport.icehockey.IceHockeyLineBuilder;
-=======
-import java.util.stream.Stream;
->>>>>>> refs/remotes/origin/main:src/Demo.java
 
 public class Demo {
     public static void main(String[] args) {
-<<<<<<< HEAD:src/betline/Test.java
 
         IceHockeyLineBuilder builder = new IceHockeyLineBuilder();
 
         builder.addWinner();
         builder.addThreeWay();
         builder.addTotal(5.5);
-        builder.addHomeHandicap(-1.5);
-        builder.addGuestHandicap(-2.5);
 
-=======
-        IceHockeyGenerator generator = new IceHockeyGenerator();
-        LineBuilder<IceHockeyGame> builder = new LineBuilder<>(generator);
-        builder.addUnit(new BetUnit<>("Победа 1 в матче",
-                game -> game.getHomeEvents().stream().unordered().anyMatch(he -> he.getEvent() == IceHockeyEvent.WIN),
-                game -> game.getGuestEvents().stream().unordered().anyMatch(ge -> ge.getEvent() == IceHockeyEvent.WIN),
-                "Победа 2 в матче"));
-        builder.addUnit(new BetUnit<>("Будет гол в пустые",
-                game -> Stream.concat(game.getHomeEvents().stream(), game.getGuestEvents().stream())
-                        .unordered().anyMatch(ge -> ge.getEvent() == IceHockeyEvent.ENG)));
->>>>>>> refs/remotes/origin/main:src/Demo.java
-        System.out.println(builder.buildLine(new IceHockeyGame()));
+        builder.buildLine(new IceHockeyGame()).stream().unordered()
+            .forEach(System.out::println);
     }
 }
--- src/betline/sport/icehockey/IceHockeyConfig.java
+++ src/betline/sport/icehockey/IceHockeyConfig.java
@@ -18,11 +18,84 @@ public class IceHockeyConfig {
     private double correction5x6 = 7.5;
     private double correction6x5 = 1.5;
 
-    private int shootoutLength = 5;
-    private double shootoutScoreProbability = 0.35;
+    public double getHomeExpected() {
+        return homeExpected;
+    }
+
+    public void setHomeExpected(double homeExpected) {
+        this.homeExpected = homeExpected;
+    }
+
+    public double getGuestExpected() {
+        return guestExpected;
+    }
+
+    public void setGuestExpected(double guestExpected) {
+        this.guestExpected = guestExpected;
+    }
+
+    public int getCurrentSecond() {
+        return currentSecond;
+    }
+
+    public void setCurrentSecond(int currentSecond) {
+        this.currentSecond = currentSecond;
+    }
+
+    public int getEmptyDifference() {
+        return emptyDifference;
+    }
+
+    public void setEmptyDifference(int emptyDifference) {
+        this.emptyDifference = emptyDifference;
+    }
+
+    public int getEmptyDuration() {
+        return emptyDuration;
+    }
+
+    public void setEmptyDuration(int emptyDuration) {
+        this.emptyDuration = emptyDuration;
+    }
+
+    public int getGameDuration() {
+        return gameDuration;
+    }
+
+    public void setGameDuration(int gameDuration) {
+        this.gameDuration = gameDuration;
+    }
 
     public int getGameWithOtDuration() {
         return gameWithOtDuration;
+    }
+
+    public void setGameWithOtDuration(int gameWithOtDuration) {
+        this.gameWithOtDuration = gameWithOtDuration;
+    }
+
+    public double getCorrection3x3() {
+        return correction3x3;
+    }
+
+    public void setCorrection3x3(double correction3x3) {
+        this.correction3x3 = correction3x3;
+    }
+
+    public double getCorrection5x6() {
+        return correction5x6;
+    }
+
+    public void setCorrection5x6(double correction5x6) {
+        this.correction5x6 = correction5x6;
+    }
+
+    public double getCorrection6x5() {
+        return correction6x5;
+    }
+
+    public void setCorrection6x5(double correction6x5) {
+        this.correction6x5 = correction6x5;
     }
 
     public int getShootoutLength() {
@@ -41,89 +114,8 @@ public class IceHockeyConfig {
         this.shootoutScoreProbability = shootoutScoreProbability;
     }
 
-    public void setGameWithOtDuration(int gameWithOtDuration) {
-        this.gameWithOtDuration = gameWithOtDuration;
-    }
+    private int shootoutLength = 5;
+    private double shootoutScoreProbability = 0.35;
 
-    public double getCorrection3x3() {
-        return correction3x3;
-    }
 
-    public void setCorrection3x3(double correction3x3) {
-        this.correction3x3 = correction3x3;
-
-    public double getCorrection6x5() {
-        return correction6x5;
-    }
-
-    public double getCorrection5x6() {
-        return correction5x6;
-    }
-
-    public void setCorrection5x6(double correction5x6) {
-        this.correction5x6 = correction5x6;
-    }
-
-    public double getCorrection6x5() {
-        return correction6x5;
-    }
-
-    public void setCorrection6x5(double correction6x5) {
-        this.correction6x5 = correction6x5;
-
-    public void setCorrection6x5(double correction6x5) {
-      this.correction6x5 = correction6x5;
-    }
-
-    public void setCorrection5x6(double correction5x6) {
-      this.correction5x6 = correction5x6;
-    }
-
-    public int getGameDuration() {
-        return gameDuration;
-    }
-
-    public void setGameDuration(int gameDuration) {
-        this.gameDuration = gameDuration;
-    }
-
-    public void setHomeExpected(double homeExpected) {
-        this.homeExpected = homeExpected;
-    }
-
-    public void setGuestExpected(double guestExpected) {
-        this.guestExpected = guestExpected;
-    }
-
-    public void setCurrentSecond(int currentSecond) {
-        this.currentSecond = currentSecond;
-    }
-
-    public void setEmptyDifference(int emptyDifference) {
-        this.emptyDifference = emptyDifference;
-    }
-
-    public void setEmptyDuration(int emptyDuration) {
-        this.emptyDuration = emptyDuration;
-    }
-
-    public double getHomeExpected() {
-        return homeExpected;
-    }
-
-    public double getGuestExpected() {
-        return guestExpected;
-    }
-
-    public int getCurrentSecond() {
-        return currentSecond;
-    }
-
-    public int getEmptyDifference() {
-        return emptyDifference;
-    }
-
-    public int getEmptyDuration() {
-        return emptyDuration;
-    }
 }
